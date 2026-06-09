81d90c7889ed1a88aac8efdf779cbdce1f903b2a
vitthal-20 <25121cn097@glbitm.ac.in>
2026-06-09 14:05:33 +0530

    Delete lines 1-12

--- day1/q1.c
+++ day1/q1.c
@@ -1,16 +1,4 @@
-#include <stdio.h>
-
-int main()
-{
-    int n,i,sum=0;
-
-    printf("Enter n: ");
-    scanf("%d",&n);
-
-    for(i=1;i<=n;i++)
-    {
-        sum=sum+i;
-    }
+     }
 
     printf("Sum = %d",sum);
 

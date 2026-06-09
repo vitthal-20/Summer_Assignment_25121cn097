d57930944542f4973edcc512a1dd3bfb0a922e5d
vitthal-20 <25121cn097@glbitm.ac.in>
2026-06-09 13:56:45 +0530

    first question

--- day1/q1.c
+++ day1/q1.c
@@ -1,12 +1,18 @@
-
 #include <stdio.h>
 
-int main(void) {
-	int n;
-	long long sum = 0;
-	if (printf("Enter a positive integer N: ") && fflush(stdout), scanf("%d", &n) == 1) {
-		for (int i = 1; i <= n; ++i) sum += i;
-		printf("Sum of first %d natural numbers = %lld\n", n, sum);
-	}
-	return 0;
+int main()
+{
+    int n,i,sum=0;
+
+    printf("Enter n: ");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++)
+    {
+        sum=sum+i;
+    }
+
+    printf("Sum = %d",sum);
+
+    return 0;
 }

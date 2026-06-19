154bf5aebbc9d47f719f4321ee69d28e782349b9
vitthal-20 <25121cn097@glbitm.ac.in>
2026-06-19 17:53:33 +0530

    DAY 19

---
+++ DAY 19/Q76.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[10][10];
+    int n,i,j,sum=0;
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
+            scanf("%d",&a[i][j]);
+
+    for(i=0;i<n;i++)
+        sum=sum+a[i][i];
+
+    printf("%d",sum);
+
+    return 0;
+}
